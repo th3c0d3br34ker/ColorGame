488b4fbbec7f30a82d5375718075dff70360f7ae
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-05-23 07:00:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const {app, BrowserWindow} = require("electron")
+const {app, BrowserWindow} = require("electron");
 const path = require("path");
 
 function createWindow()
 {
     var window = new BrowserWindow(
     {
-        webPreferences: {preload: path.join(__dirname, 'colorgame.js')},
+        webPreferences: {preload: path.join(__dirname, "colorgame.js")},
         show: false
     });
 
-    window.loadFile(path.join(__dirname, 'colorgame.html'));
-    window.maximize()   
-    window.show()
+    window.loadFile(path.join(__dirname, "colorgame.html"));
+    window.maximize();
+    window.show();
     
-    window.on('closed', () => {window = null;});
+    window.on("closed", () => {
+        window = null;
+    });
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
