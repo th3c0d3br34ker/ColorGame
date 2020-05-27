681ff66685d17795981f6420fca5caf6625c578e
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-05-27 13:46:58 +0530

    Codacy Improvements (#5)
    * Codacy Improvements
    
    * Codacy Improvements
    
    * Codacy Improvements
    
    * Codacy Improvements

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
-function createWindow()
-{
-    var window = new BrowserWindow(
-    {
-        webPreferences: {preload: path.join(__dirname, "colorgame.js")},
-        show: false
-    });
+function createWindow() {
+  var window = new BrowserWindow({
+    webPreferences: { preload: path.join(__dirname, "colorgame.js") },
+    show: false,
+  });
 
-    window.loadFile(path.join(__dirname, "colorgame.html"));
-    window.maximize();
-    window.show();
-    
-    window.on("closed", () => {
-        window = null;
-    });
+  window.loadFile(path.join(__dirname, "colorgame.html"));
+  window.maximize();
+  window.show();
+
+  window.on("closed", () => {
+    window = null;
+  });
 }
 
 app.whenReady().then(createWindow);
